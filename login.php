91df0bd06cd5e3092f7ddfed620ffe9f69074a36
Thulunga Basumatary <thulunga_b170399cs@nitc.ac.in>
2019-10-03 21:04:01 +0530

    Update login.php

--- login.php
+++ login.php
@@ -4,6 +4,32 @@
 	<title>Login</title>
 </head>
 <body>
+	<html>
+<head>
+<link rel="stylesheet" type="text/css" href="">
+</head>
+<body>
+<div class="form-wrapper">
+  
+  <form action="#" method="post">
+    <h3>Login here</h3>
+	
+    <div class="form-item">
+		<input type="text" name="user" required="required" placeholder="Username" autofocus required></input>
+    </div>
+    
+    <div class="form-item">
+		<input type="password" name="pass" required="required" placeholder="Password" required></input>
+    </div>
+    
+    <div class="button-panel">
+		<input type="submit" class="button" title="Log In" name="login" value="Login"></input>
+    </div>
+  </form>
+  
+</div>
 
+</body>
+</html>
 </body>
 </html>
